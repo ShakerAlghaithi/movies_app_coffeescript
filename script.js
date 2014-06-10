a761d1312f6af8515a8bc900c775c3b5a3c654ec
Anil Bridgpal <bridgpal@gmail.com>
2014-06-10 10:40:16 -0700

    Retrieved JSON results.

--- script.js
+++ script.js
@@ -5,8 +5,23 @@ $(function () {
 
   $("#searchButton").on("click", function(e) {
     e.preventDefault();
+
     var userInput = $("#searchTerm").val();
-    console.log(userInput);
+
+    var request = {
+      url: "http://www.omdbapi.com/",
+      type: "get",
+      data: {s: userInput}
+    }
+
+    var response = $.ajax(request)
+
+    response.done(function (data) {
+      console.log(data);
+    });
+
+
+
   })
 
 
